b750583eb59e50631c0eab0614688b33824a98c9
jjwmezun <jjwmezun@gmail.com>
2019-04-17 20:45:28 -0700

    Finish behavior for layers in Donut Dungeon

--- src/block_components/block_component_layer.cpp
+++ src/block_components/block_component_layer.cpp
@@ -8,5 +8,5 @@ BlockComponentLayer::~BlockComponentLayer() {};
 void BlockComponentLayer::interact( const Collision& collision, Sprite& sprite, Block& block, BlockType& type, Level& level, EventSystem& events, Camera& camera, Health& health, BlockSystem& blocks, SpriteSystem& sprites ) const
 {
 	sprite.layer_ = layer_;
-	sprite.graphics_->priority_ = layer_ == 1;
+	sprite.graphics_->priority_ = layer_ == 2;
 };
