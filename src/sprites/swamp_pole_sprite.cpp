0bbce35ce069792b34924c8d4fad2cd853279a0e
J. J. W. Mezun <jjwmezun@gmail.com>
2019-12-28 14:44:52 -0800

    Update swamp levels

--- src/sprites/swamp_pole_sprite.cpp
+++ src/sprites/swamp_pole_sprite.cpp
@@ -1,11 +1,12 @@
-#include "swamp_pole_sprite.hpp"
+#include "camera.hpp"
 #include "collision.hpp"
 #include "sprite_graphics.hpp"
 #include "sprite_system.hpp"
+#include "swamp_pole_sprite.hpp"
 
 SwampPoleSprite::SwampPoleSprite( int x, int y, int id )
 :
-	Sprite( std::make_unique<SpriteGraphics> ( "sprites/swamp-pole.png" ), x, y, 16, 448, { SpriteType::SWAMP_POLE }, 25, 1000, 0, 0, Direction::Horizontal::__NULL, Direction::Vertical::__NULL, nullptr, SpriteMovement::Type::FLOATING, CameraMovement::RESET_OFFSCREEN_AND_AWAY ),
+	Sprite( std::make_unique<SpriteGraphics> ( "sprites/swamp-pole.png" ), x, y, 16, 448, { SpriteType::SWAMP_POLE }, 25, 1000, 0, 0, Direction::Horizontal::__NULL, Direction::Vertical::__NULL, nullptr, SpriteMovement::Type::FLOATING, CameraMovement::PERMANENT ),
     pair_ ( nullptr )
 {
     misc_.id = id;
@@ -15,18 +16,6 @@ SwampPoleSprite::~SwampPoleSprite() {};
 
 void SwampPoleSprite::customUpdate( Camera& camera, Map& lvmap, EventSystem& events, SpriteSystem& sprites, BlockSystem& blocks, Health& health )
 {
-    if ( pair_ == nullptr )
-    {
-        for ( const std::unique_ptr<Sprite>& sprite : sprites.getSpritesList() )
-        {
-            if ( sprite->hasType( SpriteType::SWAMP_POLE ) && sprite->misc_.id == misc_.id && sprite.get() != this )
-            {
-                pair_ = sprite.get();
-                break;
-            }
-        }
-    }
-
     if ( !collide_bottom_ )
     {
         acceleration_y_ = 0;
@@ -37,14 +26,32 @@ void SwampPoleSprite::customUpdate( Camera& camera, Map& lvmap, EventSystem& eve
         vy_ = 0;
         hit_box_.y = bottomLimit();
     }
+
+    if ( pair_ == nullptr )
+    {
+        for ( const std::unique_ptr<Sprite>& sprite : sprites.getSpritesList() )
+        {
+            if ( sprite->hasType( SpriteType::SWAMP_POLE ) && sprite->misc_.id == misc_.id && sprite.get() != this )
+            {
+                pair_ = sprite.get();
+                break;
+            }
+        }
+
+        if ( camera.offscreen( hit_box_ ) && camera.offscreen( pair_->hit_box_ ) )
+        {
+            reset();
+            pair_->reset();
+        }
+    }
 };
 
 void SwampPoleSprite::customInteract( Collision& my_collision, Collision& their_collision, Sprite& them, BlockSystem& blocks, SpriteSystem& sprites, Map& lvmap, Health& health, EventSystem& events )
 {
-	if ( them.hasType( SpriteType::HERO ) && their_collision.overlapYBottom() > 0 && their_collision.overlapYBottom() < 8000 )
+	if ( pair_ != nullptr && them.hasType( SpriteType::HERO ) && their_collision.overlapYBottom() > 0 && their_collision.overlapYBottom() < 8000 )
 	{
         them.collideStopAny( their_collision );
-        if ( hit_box_.y < bottomLimit() )
+        if ( hit_box_.y < bottomLimit() || pair_->hit_box_.y > pair_->original_hit_box_.y - Unit::BlocksToSubPixels( 3 ) )
         {
             moveDown();
             collide_bottom_ = true;
